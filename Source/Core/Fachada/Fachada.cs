eeffd681f189cffb99e49b0405922eb8fdfb7ef4
spectrefelsip <felsip@gmail.com>
2016-11-19 22:23:20 -0500

    Se carga la carpeta de manejo de transacciones
    Se agrega el proyecto core de transacciones con la bd

---
+++ Source/Core/Fachada/Fachada.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+
+namespace Core.BigData.ManejadorBD
+{
+    public interface IFachada
+    {
+        /// <summary>
+        /// Ejecuta una consulta y retorna un dataTable
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        DataTable GetDataTable(string query);
+
+        /// <summary>
+        /// Ejecuta una insercion
+        /// </summary>
+        /// <param name="query"></param>
+        void ExecuteNonQuery(string query);
+
+        /// <summary>
+        /// Ejecuta una actualizacion
+        /// </summary>
+        /// <param name="query"></param>
+        void ExecuteNonQueryUpdate(string query);
+
+        /// <summary>
+        /// Ejecuta una consulta y devuelve un valor entero
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        int ExecuteScalar(string query);
+
+        /// <summary>
+        /// Salva la imagen en la bd
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="array"></param>
+        void CargarImagen(string query, byte[] array);
+
+        /// <summary>
+        /// Convierte una fecha en formato yyyy/MM/dd
+        /// </summary>
+        /// <param name="fecha"></param>
+        /// <returns></returns>
+        string convertirFecha(DateTime fecha);
+    }
+}
---
+++ Source/Core/Implementacion/Implementacion.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+
+namespace Core.BigData.ManejadorBD
+{
+    public class Implementacion : IFachada
+    {
+        //Obtengo la cadena de conexion que se crea desde el web.config para usarla globalmente desde esta clase
+        private string conexion = ConfigurationManager.ConnectionStrings["DataSourceBaseDatos"].ConnectionString;
+
+        public DataTable GetDataTable(string query)
+        {            
+            SqlConnection conn = new SqlConnection(conexion);
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = new SqlCommand(query, conn);
+            DataTable myDataTable = new DataTable();
+            conn.Open();
+            try
+            {
+                adapter.Fill(myDataTable);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return myDataTable;
+        }
+
+        public void ExecuteNonQuery(string query)
+        {
+            SqlConnection conn = new SqlConnection(conexion);
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            try
+            {
+                conn.Open();
+                adapter.InsertCommand = new SqlCommand(query, conn);
+                adapter.InsertCommand.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                adapter.InsertCommand.Transaction.Rollback();
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void ExecuteNonQueryUpdate(string query)
+        {
+            SqlConnection conn = new SqlConnection(conexion);
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            try
+            {
+                conn.Open();
+                adapter.UpdateCommand = new SqlCommand(query, conn);
+                adapter.UpdateCommand.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                adapter.UpdateCommand.Transaction.Rollback();
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public int ExecuteScalar(string query)
+        {
+            SqlConnection conn = new SqlConnection(conexion);
+            SqlCommand comando = new SqlCommand(query, conn);
+            int dato = 0;
+            try
+            {
+                conn.Open();
+                dato = Convert.ToInt32(comando.ExecuteScalar());
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dato;
+        }
+
+        public void CargarImagen(string query, byte []array)
+        {
+            SqlConnection conn = new SqlConnection(conexion);
+            SqlCommand comando = new SqlCommand(query,conn);
+            try
+            {
+                MemoryStream ms = new MemoryStream();
+                //img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                //ms.ToArray();
+                byte[] imgbyte = array;
+                //comando.Text="Insert Into [Image] Values @image";
+                comando.Parameters.AddWithValue("@imagen",imgbyte );
+                conn.Open();
+                comando.ExecuteNonQuery();
+                comando.Dispose();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conn.Close();                
+                comando.Dispose();
+            }         
+        }
+
+        public DataTable ExcuteSP(string numeroDoc)
+        {
+            SqlConnection conn = new SqlConnection(conexion);
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            SqlCommand cmd = new SqlCommand();
+            DataTable Tabla = new DataTable();
+            conn.Open();
+            try
+            {
+                conn.BeginTransaction();
+                cmd = new SqlCommand("SPResultadoExamen", conn);
+                cmd.Parameters.Add(new SqlParameter("@NumeroDoc", numeroDoc));
+                cmd.CommandType = CommandType.StoredProcedure;
+                adapter.SelectCommand = cmd;
+                adapter.Fill(Tabla);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return Tabla;
+        }
+
+        public string convertirFecha(DateTime fecha)
+        {
+            return fecha.Year + "-" + fecha.Month + "-" + fecha.Day;
+        }
+    }
+}
